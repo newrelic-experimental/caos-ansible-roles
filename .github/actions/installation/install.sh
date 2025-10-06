d43b6a889a74c7da54f911dcb88ffeea924bc77c
mbolla22 <mbolla@newrelic.com>
2025-10-06 14:29:17 +0530

    Added suse 15.7

--- .github/actions/installation/install.sh
+++ .github/actions/installation/install.sh
@@ -1,6 +1,6 @@
 sudo pipx uninstall ansible-core
 sudo pip3 install 'rich>=10.0.0,<11.0.0'
-sudo pip3 install ansible-lint[community]==24.9.2
+sudo pip3 install ansible-lint[community]==5.3.2
 
 
 ansible --version
