0cd8d977174b0fe2e269f8361be9156e23702358
mbolla22 <mbolla@newrelic.com>
2025-10-07 11:43:11 +0530

    Added --ignore-installed flag

--- .github/actions/installation/install.sh
+++ .github/actions/installation/install.sh
@@ -1,6 +1,6 @@
 sudo pipx uninstall ansible-core
-sudo pip3 install 'rich>=10.0.0,<11.0.0'
-sudo pip3 install ansible-lint==6.0.2
+sudo pip3 install --ignore-installed 'rich>=10.0.0,<11.0.0'
+sudo pip3 install --ignore-installed ansible-lint==6.0.2
 
 # Verify installations
 ansible --version
