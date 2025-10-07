8c62919958081f1f76b8c70aa0deb5a78fbef936
mbolla22 <mbolla@newrelic.com>
2025-10-07 11:12:16 +0530

    Updated ansible-lint version

--- .github/actions/installation/install.sh
+++ .github/actions/installation/install.sh
@@ -1,4 +1,7 @@
-sudo pipx uninstall ansible-core
+sudo pipx uninstall ansible-core || true
+
+# Install with --ignore-installed to skip system packages
+sudo pip3 install --ignore-installed ansible-core>=2.12.0
 sudo pip3 install 'rich>=10.0.0,<11.0.0'
 sudo pip3 install ansible-lint==24.9.2
 
