f7c4a77b72f1f20b75b07da342ad251aa796a094
mbolla22 <mbolla@newrelic.com>
2025-10-07 11:26:43 +0530

    Added pyYAML install logic

--- .github/actions/installation/install.sh
+++ .github/actions/installation/install.sh
@@ -1,6 +1,8 @@
 sudo pipx uninstall ansible-core
+sudo pip3 install --force-reinstall --no-deps PyYAML
 sudo pip3 install 'rich>=10.0.0,<11.0.0'
 sudo pip3 install ansible-lint==6.10.0
+export PATH="/usr/local/bin:$PATH"
 
 # Verify installations
 ansible --version
