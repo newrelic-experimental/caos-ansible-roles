029c981d1e0918aea91c588f09c8f795c3b041fd
mbolla22 <mbolla@newrelic.com>
2025-10-07 12:00:11 +0530

    Chnaged ubuntu version

--- .github/actions/installation/install.sh
+++ .github/actions/installation/install.sh
@@ -1,10 +1,7 @@
-sudo pipx uninstall ansible-core || true
+sudo pipx uninstall ansible-core
+sudo pip3 install 'rich>=10.0.0,<11.0.0'
+sudo pip3 install ansible-lint[community]==5.3.2
 
-# Use versions that work together
-sudo pip3 install --ignore-installed ansible-core==2.15.12
-sudo pip3 install --ignore-installed 'rich>=10.0.0,<11.0.0'
-sudo pip3 install --ignore-installed ansible-lint==6.22.2
 
-# Verify installations
 ansible --version
 ansible-lint --version
